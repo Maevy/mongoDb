b69663d8e80254bf120498748e5b569448d80121
gorski <maciej.gorski@gvl.de>
2022-08-17 12:37:31 +0200

    starting point

--- src/main/java/com/example/mongodb/domain/abschluss/AbschlussRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.mongodb.domain.abschluss;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface AbschlussRepository extends MongoRepository<AbschlussEntity, String> {
-
-}
--- src/main/java/com/example/mongodb/domain/wphg/ChecklistenRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.mongodb.domain.wphg;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChecklistenRepository extends JpaRepository<ChecklisteEntity, Long> {
-
-}
