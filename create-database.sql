ab424a7296690c765f9e8c94bfaf7d1b42673595
gorski <maciej.gorski@gvl.de>
2022-08-17 12:27:45 +0200

    added create script

---
+++ create-database.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE ourdb;
+GO
+USE ourdb;
+GO
+create table wphg_abschluss
+(
+    id                                     bigint not null
+        primary key,
+    abschluss_art                          varchar(255)
+)
+    GO
+INSERT INTO wphg_abschluss (id, abschluss_art) VALUES (5, N'ABSCHLUSS_OHNE_GENEHMIGUNG');
+create table wphg_checkliste
+(
+    id                                                         bigint not null
+        primary key,
+    created_date                                               datetime2,
+    deleted_date                                               datetime2,
+    name                                                       varchar(max)
+)
+    go
+INSERT INTO wphg_checkliste (id, created_date, deleted_date, letzte_fachliche_bearbeitung_datum) VALUES (1, N'2022-08-08 12:22:29.1255515', null, N'Geile Checkliste');
+    go
+create table wphg_prozess
+(
+    id                                                         bigint not null
+        primary key,
+    created_date                                               datetime2,
+    deleted_date                                               datetime2,
+    name                                                       varchar(max)
+)
+    go
+INSERT INTO wphg_prozess (id, created_date, deleted_date,name) VALUES (1, N'2022-08-08 12:20:30.6293812', N'2022-08-15 15:21:07.5342206', N'Maciej Gorski');
+
